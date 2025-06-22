b255ce9dfa0b0fa6e097ef03cc5b03204aff8798
RegularCoder890 <cornelius.klima@gmail.com>
2025-06-22 17:58:02 +0200

    Runtime files

---
+++ run.sh
@@ -0,0 +1,3 @@
+set -e
+sh build.sh
+qemu-system-x86_64 -bios /usr/share/ovmf/x64/OVMF.4m.fd -kernel target/x86_64-unknown-uefi-debug/debug/Secondtry.efi -d guest_errors,invalid_mem -m size=1G -smp cores=2 -debugcon stdio -cpu max -d int -M q35 -net nic,model=rtl8139 -no-reboot -drive file=disk.img,if=none,id=nvm -device nvme,serial=deadbeef,drive=nvm -D logfile.log
