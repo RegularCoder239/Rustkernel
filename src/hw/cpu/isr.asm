73eafeb315bb8f02d151a78a854bbe13e9d97d8a
RegularCoder239 <217169551+RegularCoder239@users.noreply.github.com>
2025-06-24 06:13:45 +0200

    Remove 2 assembler files

--- src/hw/cpu/isr.asm
+++
@@ -1,6 +0,0 @@
-section .isr_text
-
-nopISR:
-	mov rax, 0x0
-	mov cr2, rax
-	iretq
--- src/hw/cpu/tools.asm
+++
@@ -1,9 +0,0 @@
-global handleNopInterrupt
-
-section .text
-handleNopInterrupt:
-	push rax
-	mov ax, 0x20
-	out 0x20, ax
-	pop rax
-	iretq
